93a1dac090ac90d96c9d2da6254d7bd84cae9b52
Joshua Eigbe <jeigbe@gmail.com>
2024-01-31 14:26:20 +0100

    fix: updated allowed origins

--- config/allowedOrigns.js
+++ config/allowedOrigns.js
@@ -3,13 +3,13 @@
  * @Github: https://github.com/jsh007
  * @Date: 2024-01-04 19:01:21
  * @LastEditors: Joshua Eigbe jeigbe@gmail.com
- * @LastEditTime: 2024-01-31 13:57:16
+ * @LastEditTime: 2024-01-31 14:25:57
  * @FilePath: /quicktickets_backend/config/allowedOrigns.js
  * @copyrightText: Copyright (c) Joshua Eigbe. All Rights Reserved.
  * @Description: See Github repo
  */
 const allowedOrigns = [
-  "https://quicktickets.onrender.com/",
+  "https://quicktickets.onrender.com",
   // "http://localhost:3000",
   // "http://localhost:5173",
 ];
