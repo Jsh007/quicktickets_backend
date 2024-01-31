619dc56e483377166cf70a4dda0cd04650131106
Joshua Eigbe <jeigbe@gmail.com>
2024-01-31 14:19:42 +0100

    fix: updated allowed origins

--- config/allowedOrigns.js
+++ config/allowedOrigns.js
@@ -2,16 +2,16 @@
  * @Author: Joshua Eigbe self@joshuaeigbe.com
  * @Github: https://github.com/jsh007
  * @Date: 2024-01-04 19:01:21
- * @LastEditors: Joshua Eigbe self@joshuaeigbe.com
- * @LastEditTime: 2024-01-11 15:42:48
- * @FilePath: /mern-crud/config/allowedOrigns.js
+ * @LastEditors: Joshua Eigbe jeigbe@gmail.com
+ * @LastEditTime: 2024-01-31 13:57:16
+ * @FilePath: /quicktickets_backend/config/allowedOrigns.js
  * @copyrightText: Copyright (c) Joshua Eigbe. All Rights Reserved.
  * @Description: See Github repo
  */
 const allowedOrigns = [
-  "http://localhost:3000",
-  "https://google.com",
-  "http://localhost:5173",
+  "https://quicktickets.onrender.com/",
+  // "http://localhost:3000",
+  // "http://localhost:5173",
 ];
 
 module.exports = allowedOrigns;
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,9 +2,9 @@
  * @Author: Joshua Eigbe self@joshuaeigbe.com
  * @Github: https://github.com/jsh007
  * @Date: 2024-01-04 19:01:30
- * @LastEditors: Joshua Eigbe self@joshuaeigbe.com
- * @LastEditTime: 2024-01-23 11:18:07
- * @FilePath: /mern-crud/config/corsOptions.js
+ * @LastEditors: Joshua Eigbe jeigbe@gmail.com
+ * @LastEditTime: 2024-01-31 13:58:55
+ * @FilePath: /quicktickets_backend/config/corsOptions.js
  * @copyrightText: Copyright (c) Joshua Eigbe. All Rights Reserved.
  * @Description: See Github repo
  */
